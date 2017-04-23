095aca10c5cf7c36bc3e937f9f036031b8372df7
Zachary Bergmann <zbbergma@gmail.com>
2017-04-23 18:00:54 -0500

    (maint) Fix a lint on server.js and mis-spelling on index.html

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
