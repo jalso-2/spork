0a3a17322a40a49e3e9b327514369658f680ba31
Zachary Bergmann <zbbergma@gmail.com>
2017-04-24 17:53:47 -0500

    (feat) Add dependencies and template out base for app
    -Add webpack and webpack-dev config files to project
    -Create public directory with initial files
    -Set up react-router and confirm navigation between pages
    -Test dev server on localhost, works
    -Add several dependendencies for project
    -Fix server.js to work with webpack

---
+++ public/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link, IndexLink } from 'react-router';
+
+const App = props => (
+  <div>
+    <nav>
+      <li><IndexLink to="/">Home</IndexLink></li>
+      <li><Link to="/contact">Contact</Link></li>
+    </nav>
+    {props.children}
+  </div>
+);
+
+export default App;
