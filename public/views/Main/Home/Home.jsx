123c58be9831a7a47ab7451833652178fd014788
Zachary Bergmann <zbbergma@gmail.com>
2017-04-29 13:50:52 -0500

    (feat) Add logout functionality to auth with multi-page nav

--- public/views/Main/Home/Home.jsx
+++ public/views/Main/Home/Home.jsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import AuthService from '../../../utils/AuthService';
+import Nav from '../Nav/Nav';
 
-export class Home extends React.Component {
-
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      profile: props.auth.getProfile(),
-    };
-    props.auth.on('profile_updated', (newProfile) => {
-      this.setState({ profile: newProfile });
-    });
-  }
-
-  logout() {
-    this.props.auth.logout();
-    this.context.router.push('/login');
-  }
-
-  render() {
-    const { profile } = this.state;
-    return (
-      <div>
-        <h2>Home</h2>
-        {console.log(profile)}
-        <p>Welcome {profile.name}!</p>
-        <Button onClick={this.logout.bind(this)}>Logout</Button>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div>
+    <Nav />
+    <h2>Got this one!</h2>
+  </div>
+);
 
 export default Home;
